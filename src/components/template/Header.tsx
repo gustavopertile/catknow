9d212cca596a6b744891e72e241b1fa9600eeda4
gustavopertile <gu.follador@gmail.com>
2025-05-07 15:48:38 -0300

    implement auto-hide header

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,7 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const router = useRouter();
+  const [visible, setVisible] = useState(true);
+  const [lastScroll, setLastScroll] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const currentScroll = window.scrollY;
+
+      if (currentScroll > lastScroll && currentScroll > 80) {
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+
+      setLastScroll(currentScroll);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [lastScroll]);
+
   return (
-    <header className="flex justify-between">
-      <h1 className="text-3xl font-bold mb-4">🐈 CATKNOW</h1>
+    <header
+      className={`sticky top-0 z-50 transition-transform duration-300 ${
+        visible ? "translate-y-0" : "-translate-y-full"
+      } flex justify-center 
+       rounded-b-lg p-8 md:p-10 shadow bg-background`}
+    >
+      <div onClick={() => router.push("/")}>
+        <h1 className="text-4xl md:text-6xl font-bold cursor-pointer">
+          🐈 CATKNOW
+        </h1>
+      </div>
     </header>
   );
 }
+
+// bg-gradient-to-r from-foreground to-70% to-background rounded-b-lg p-6 border border-t-0`}
