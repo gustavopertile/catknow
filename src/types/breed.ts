7b1f14f44c2888307f8b585e6b928dd493995ecc
gustavopertile <gu.follador@gmail.com>
2025-05-07 15:48:50 -0300

    update breed type

--- src/types/breed.ts
+++ src/types/breed.ts
@@ -1,10 +1,40 @@
 export type Breed = {
-  weight: string;
+  weight?: {
+    imperial?: string;
+    metric?: string;
+  };
   id: string;
-  temperament: string;
-  origin: string;
-  country_codes: string;
-  country_code: string;
-  life_span: string;
-  wikipedia_url: string;
+  name: string;
+  temperament?: string;
+  origin?: string;
+  country_codes?: string;
+  country_code?: string;
+  life_span?: string;
+  description?: string;
+  wikipedia_url?: string;
+  alt_names?: string;
+  cfa_url?: string;
+
+  adaptability?: number;
+  affection_level?: number;
+  child_friendly?: number;
+  dog_friendly?: number;
+  energy_level?: number;
+  grooming?: number;
+  health_issues?: number;
+  intelligence?: number;
+  shedding_level?: number;
+  social_needs?: number;
+  stranger_friendly?: number;
+  vocalisation?: number;
+  indoor?: number;
+  lap?: number;
+  experimental?: number;
+  hairless?: number;
+  natural?: number;
+  rare?: number;
+  rex?: number;
+  suppressed_tail?: number;
+  short_legs?: number;
+  hypoallergenic?: number;
 };
