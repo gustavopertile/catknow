50a355d09cb5563e60616e1d25ee656ca7d854d2
gustavopertile <gu.follador@gmail.com>
2025-05-07 15:47:48 -0300

    update breed description rendering

---
+++ src/components/info/BreedDescription.tsx
@@ -0,0 +1,38 @@
+import { Breed } from "@/types/breed";
+
+type Props = {
+  breed: Breed;
+};
+
+export default function BreedDescription({ breed }: Props) {
+  const renderInfo = (label: string, info: string | undefined) => {
+    return (
+      <p>
+        <strong>{label}:</strong> {info ?? "Unknown"}
+      </p>
+    );
+  };
+
+  return (
+    <>
+      {renderInfo("Name", breed?.name)}
+      {renderInfo("Description", breed?.description)}
+      {renderInfo("Life Span", breed?.life_span)}
+      {renderInfo("Origin", breed?.origin)}
+
+      {breed?.wikipedia_url && (
+        <p>
+          <span className="font-semibold">More Info:</span>{" "}
+          <a
+            href={breed.wikipedia_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-primary"
+          >
+            Wikipedia
+          </a>
+        </p>
+      )}
+    </>
+  );
+}
